7337fda603e798c3dc2a91677ca93e82474b6751
kei <kei@omise.co>
2017-01-05 12:20:38 +0900

    omise.js embedded: still in the state that child node of div class cannot be retrieved

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,9 @@ class CartsController < ApplicationController
   def new
   end
   def create
+    # render plain text
+    # params gained from view/carts/new.html.erb
+    render plain: params[:cart].inspect
   end
   def show
   end
